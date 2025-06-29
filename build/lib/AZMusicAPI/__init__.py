a91973b93887bc5b646c864e23919c500211ac05
zlk-sys <18079031125@163.com>
2025-06-29 10:21:12 +0800

    1.6.0

--- build/lib/AZMusicAPI/__init__.py
+++ build/lib/AZMusicAPI/__init__.py
@@ -1,8 +1,43 @@
+import re
+from collections import Counter
 import requests
-
+from bilibili_api import search, sync, video_zone
 
 # AZ Studio版权所有
 # 该项目仅供学习研究 请勿用于违法事业 违者与AZ Studio无关
+
+def extract_keyword(text):
+    # 使用正则表达式提取所有<em class="keyword"></em>标签中的内容
+    keywords = re.findall(r'<em class="keyword">(.*?)</em>', text)
+
+    # 如果有书名号格式的标签，优先返回其中的内容
+    for keyword in keywords:
+        if '《' in keyword and '》' in keyword:
+            return keyword
+
+    # 如果没有书名号格式的标签，返回出现最多的关键词
+    if keywords:
+        most_common_keyword = Counter(keywords).most_common(1)[0][0]
+        return most_common_keyword
+
+    # 如果没有关键词，返回空字符串
+    return ""
+
+async def search_by_order(keyword, num):
+    return await search.search_by_type(
+        keyword,
+        search_type=search.SearchObjectType.VIDEO,
+        order_type=search.OrderVideo.SCORES,
+        time_range=10,
+        video_zone_type=video_zone.VideoZoneTypes.MUSIC,
+        page=num,
+    )
+
+def min_multiples_to_exceed(n):
+    multiplier = 1
+    while 17 * multiplier <= n:
+        multiplier += 1
+    return multiplier
 
 def getmusic(keyword, api, cookie="", number=20, server="ncma"):
     number = int(number)
@@ -10,7 +45,8 @@ def getmusic(keyword, api, cookie="", number=20, server="ncma"):
         'Cookie': cookie
     }
     # 获得指定关键词的歌曲 关键词可为 歌手/歌曲名/专辑名
-    # keyword：关键词 number:返回歌曲数量,可选,默认20 api:自行部署neteasecloudmusicapi的地址
+    # keyword：关键词 number:返回歌曲数量,可选,默认20 api:自行部署ncma或qqma的地址，bili留空
+    # server：可选，默认ncma，可选值：ncma，qqma，bili
     # 返回值说明：正常返回列表  "Error 0"：没有结果 "Error 1":用户未输入
     search_url = api + "search"
     if server == "ncma":
@@ -42,6 +78,31 @@ def getmusic(keyword, api, cookie="", number=20, server="ncma"):
                 return data
         else:
             return "Error 1"
+    elif server=="bili":
+        pages = min_multiples_to_exceed(number)
+        if len(keyword) > 0:
+            try:
+                res = sync(search_by_order(keyword, pages))
+            except:
+                return "NetworkError"
+            try:
+                ret = res["result"]
+            except:
+                return "Error 0"
+            if len(ret) <= 0:
+                return "Error 0"
+            else:
+                data = []
+                try:
+                    for i in ret:
+                        data.append({"id": i["bvid"], "name": extract_keyword(i["title"]), "artists": str(i["author"]).replace("《", "").replace("》", ""),"album": str(i["title"]).replace('<em class="keyword">', "").replace('</em>', "")})
+                except:
+                    data.append({"id": '-1', "name": 'error', "artists": 'error',
+                                 "album": 'error'})
+                if len(data) > number:
+                    data = data[:number]
+                return data
+
     else:
         if len(keyword) == 0:
             return "Error 1"
@@ -68,13 +129,15 @@ def getmusic(keyword, api, cookie="", number=20, server="ncma"):
 
 def geturl(id, api, cookie="", server="ncma", level="standard"):
     # 获取音乐的音频地址
-    # id:歌曲的id值 level:返回音频的音质，仅NCMA api:自行部署neteasecloudmusicapi的地址
+    # id:歌曲的id值 level:返回音频的音质，仅NCMA api:自行部署ncma或qqma的地址，bili留空
+    # server：可选，默认ncma，可选值：ncma，qqma
+    # bili渠道不能走这里
     # 返回值：正常:返回音频链接  "Error 3":id不正确或无版权  "Error 4":获取链接失败，建议检查是否登录
     headers = {
         'Cookie': cookie
     }
     if server == "ncma":
-        search_url = api + "song/url"
+        search_url = api + "song/url/v1"
         search_data = {"id": id, "level": level}
         try:
             response_data = requests.get(search_url, params=search_data, headers=headers).json()
@@ -99,3 +162,4 @@ def geturl(id, api, cookie="", server="ncma", level="standard"):
         except:
             return "Error 3"
         return song_url
+
--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from setuptools import setup
 
 setup(
     name='AZMusicAPI',
-    version='1.5.1',
+    version='1.6.0',
     author='AZ Studio',
     description='轻松访问音乐信息和歌曲音频链接 | Easy access to music information and song audio links',
     long_description=open('readme.md', encoding='utf-8').read(),
     long_description_content_type="text/markdown",
     packages=['AZMusicAPI'],
-    install_requires=['requests'],
+    install_requires=['requests', 'bilibili-api-python'],
     project_urls={
         'GitHub': 'https://github.com/AZ-Studio-2023/AZMusicAPI/'
     },
